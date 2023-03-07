deac6ffa2f4ed9e055235643bb66fee03d98c2d8
junhyeon.oh <oh.junhyeon95@gmail.com>
2023-03-07 15:54:35 +0900

    3.21.3

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,20 +12,50 @@ let package = Package(
         .library(
             name: "BuzzAdBenefit_spm",
             targets: [
-                "BuzzAdBenefit"
+                "BuzzAdBenefit",
+                "BuzzAdBenefitNative",
+                "BuzzAdBenefitInterstitial",
+                "BuzzAdBenefitNative"
             ]
         ),
         .library(
             name: "BuzzAdBenefit",
             targets: ["BuzzAdBenefit"]
         ),
+        .library(
+            name: "BuzzAdBenefitNative",
+            targets: ["BuzzAdBenefitNative"]
+        ),
+        .library(
+            name: "BuzzAdBenefitInterstitial",
+            targets: ["BuzzAdBenefitInterstitial"]
+        ),
+        .library(
+            name: "BuzzAdBenefitNative",
+            targets: ["BuzzAdBenefitNative"]
+        )
     ],
     dependencies: [],
     targets: [
         .binaryTarget(
             name: "BuzzAdBenefit",
-            url: "https://storage.googleapis.com/buzzvil-client-app/bab-ios/6112/BuzzAdBenefit.zip",
-            checksum: "c295b832b29152551074a16dd7d235df45dc902ec215043bd4f7da8a01b6503b"
+            url: "https://storage.googleapis.com/buzzvil-client-app/bab-ios/5898/BuzzAdBenefit.zip",
+            checksum: "acbe97c5f5959594da6597c5ebf56862c6cf43ea93866e1673aaa7615106c911"
+        ),
+        .binaryTarget(
+            name: "BuzzAdBenefitNative",
+            url: "https://storage.googleapis.com/buzzvil-client-app/bab-ios/5898/BuzzAdBenefitNative.zip",
+            checksum: "9ed306f6554376dd2d75af8573f68c4267071cd44df0f5be24b57fa2c856ec47"
+        ),
+        .binaryTarget(
+            name: "BuzzAdBenefitInterstitial",
+            url: "https://storage.googleapis.com/buzzvil-client-app/bab-ios/5898/BuzzAdBenefitInterstitial.zip",
+            checksum: "a3957f6aa67f31db3011fcb4a27ec5734f7022cf99b78da483dac76c750081bd"
+        ),
+        .binaryTarget(
+            name: "BuzzAdBenefitFeed",
+            url: "https://storage.googleapis.com/buzzvil-client-app/bab-ios/5898/BuzzAdBenefitFeed.zip",
+            checksum: "6849328d369f5686e86aa8d01fd57cbd8e060ba630b4db355deb56fb7b918df7"
         )
     ]
 )
